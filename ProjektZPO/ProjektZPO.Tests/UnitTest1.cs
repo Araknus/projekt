ffafd2adfac1eb6bf9394d725173c321ed1a0091
Maciej Lamperski <rexlampart@gmail.com>
2017-05-23 12:52:00 +0200

    test z Visuala do Githuba

---
+++ ProjektZPO/ProjektZPO.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjektZPO.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //test wrzucania bezpośrednio z Visuala na GitHuba
+        }
+    }
+}
